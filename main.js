0ebb27a5a783586654afe1f9cf6ff75508346827
Arty-Mas <mas.arty@gmail.com>
2020-01-04 22:22:06 +0200

    nonno

--- main.js
+++ main.js
@@ -1,25 +1,76 @@
-var randomNumber = Math.floor(Math.random() * 100) + 1;
-var userInt;
+var randomNumber = Math.floor(Math.random() * 101) + 1;
+var field = document.check.chislo;
+var userInt = field.value;
 var attempts;
-var result;
+var result = document.createElement("p");
 
 
 //Сравниваем введеное и загадонное
 function checkLoh (){
+if (attempts === 1) {
+    result.textContent = 'Попытка 1: ';
+}   
 if (userInt == randomNumber) {
-    lastResult.textContent = 'Вы выиграли';
+    result.textContent = 'Вы выиграли';
+    gameOver();
 }
 else if (userInt < randomNumber){
-    lastResult.textContent = 'Больше';
+    result.textContent = 'Больше';
 } 
-else  {
-    lastResult.textContent = 'Меньше';
+else if (userInt > randomNumber){
+    result.textContent = 'Меньше';
 }
+else if (attempts === 10)
+    result.textContent = 'Вы проиграли';
+    gameOver();
+
+  attempts++;
+  field.value = '';
+  field.focus();
 }
  
+function gameOver() {
+    field.disabled = true;
+    sendBut.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Начать заново';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+  }
+
+  function resetGame() {
+    attempts = 1;
+  
+    var resetParas = document.querySelectorAll('.resultParas p');
+    for (var i = 0 ; i < resetParas.length ; i++) {
+      resetParas[i].textContent = '';
+    }
+  
+    resetButton.parentNode.removeChild(resetButton);
+  
+    field.disabled = false;
+    sendBut.disabled = false;
+    field.value = '';
+    field.focus();
+  
+    //lastResult.style.backgroundColor = 'white';
+  
+    randomNumber = Math.floor(Math.random() * 101) + 1;
+  }
+//document.addEventListener ("onclick", checkAtt);
 //Проверяем количество попыток
-function checkAtt () {
-for (attempts = 1; attempts < 10; attempts ++) {
-    checkLoh ();
-}
-}
+//function checkAtt () {
+//for (attempts === 1; attempts < 10; attempts ++) {
+   // checkLoh ();
+  //  if (attempts == 10) result.textContent = 'Вы проиграли';
+    //var attemptsCount = document.createElement("p");
+    //attemptsCount = attempts.value;
+    //resultGame.appendChild(attemptsCount);
+
+//}
+
+//}
+
+resultGame.appendChild(result);
+var sendBut = document.check.send;
+sendBut.addEventListener("click", checkLoh);
