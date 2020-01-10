f48b7d4c23dc5a9b5f482fc04d4db2f04c9e18a5
Arty-Mas <mas.arty@gmail.com>
2020-01-10 17:39:45 +0200

    new

--- main.js
+++ main.js
@@ -1,76 +1,46 @@
-var randomNumber = Math.floor(Math.random() * 101) + 1;
-var field = document.check.chislo;
-var userInt = field.value;
-var attempts;
-var result = document.createElement("p");
+var randomNum = Math.floor(Math.random() *100) + 1;
+var userInput;
+var attempts = 1;
+var comparison = document.querySelector('h2');
+var inputField = document.querySelector('inputField');
 
-
-//Сравниваем введеное и загадонное
-function checkLoh (){
-if (attempts === 1) {
-    result.textContent = 'Попытка 1: ';
-}   
-if (userInt == randomNumber) {
-    result.textContent = 'Вы выиграли';
-    gameOver();
-}
-else if (userInt < randomNumber){
-    result.textContent = 'Больше';
-} 
-else if (userInt > randomNumber){
-    result.textContent = 'Меньше';
-}
-else if (attempts === 10)
-    result.textContent = 'Вы проиграли';
-    gameOver();
-
-  attempts++;
-  field.value = '';
-  field.focus();
-}
- 
-function gameOver() {
-    field.disabled = true;
-    sendBut.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Начать заново';
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', resetGame);
-  }
-
-  function resetGame() {
-    attempts = 1;
-  
-    var resetParas = document.querySelectorAll('.resultParas p');
-    for (var i = 0 ; i < resetParas.length ; i++) {
-      resetParas[i].textContent = '';
+document.getElementById('button').onclick = () => checkNum();           
+//Проверка введеного числа 1 - 100
+function checkNum() {
+    userInput = Number(inputField).value;
+    if (userInput > 0 && userInput <= 100) {
+       checkAtt();
     }
-  
-    resetButton.parentNode.removeChild(resetButton);
-  
-    field.disabled = false;
-    sendBut.disabled = false;
-    field.value = '';
-    field.focus();
-  
-    //lastResult.style.backgroundColor = 'white';
-  
-    randomNumber = Math.floor(Math.random() * 101) + 1;
-  }
-//document.addEventListener ("onclick", checkAtt);
-//Проверяем количество попыток
-//function checkAtt () {
-//for (attempts === 1; attempts < 10; attempts ++) {
-   // checkLoh ();
-  //  if (attempts == 10) result.textContent = 'Вы проиграли';
-    //var attemptsCount = document.createElement("p");
-    //attemptsCount = attempts.value;
-    //resultGame.appendChild(attemptsCount);
-
-//}
-
-//}
-
-resultGame.appendChild(result);
-var sendBut = document.check.send;
-sendBut.addEventListener("click", checkLoh);
+    else  alert('Число вне диапазона');
+    }
+//Попытка    
+function checkAtt() {
+    if (attempts >= 1 && attempts < 11) {
+        runCheckNum();
+    }
+    else gameOver();
+}
+//Совпдает ли с рандомом    
+function runCheckNum() {
+    if (userInput === randomNum) {
+        comparison.innerText = 'Угадал';
+        gameOver();
+    }
+    else loh();
+}
+//Проверка больше или меньше рандома    
+function loh() {    
+    if (userInput > randomNum) {
+        comparison.innerText = 'Меньше';
+        checkNum();
+    }
+    else if (userInput < randomNum) {
+        comparison.innerText = 'Больше';
+        checkNum();
+    }
+    attempts++;
+}
+//Конец игры
+function gameOver() {
+    alert ("one More");
+}
